9de68c6cedcee43412db91774e5e1ad76a960f13
Arascely <grissel.rodriguez.27@unsch.edu.pe>
2026-07-18 18:39:21 -0500

    Actualizar el main

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,5 +6,6 @@ const authController = require('../controllers/auth.controller');
 
 router.post('/login', authController.iniciarSesion);
 router.post('/registro', authController.registrarUsuario);
+router.get('/verificar/:token', authController.verificarCuenta);
 
 module.exports = router;
